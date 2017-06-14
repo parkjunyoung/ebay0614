137ea8ca2ae5489d659be37524787981cd5d3a3b
parkjunyoung <pjoon86@naver.com>
2017-06-14 14:14:13 +0900

    api 작성

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import path from 'path';
 import db from './models';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
  
+import admin from './routes/admin';
+
 const app = express();
  
 let port = 3000;
@@ -22,8 +26,16 @@ db.sequelize.authenticate()
         console.error('Unable to connect to the database:', err);
 });
 
+
+// logger
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // SERVE STATIC FILES - REACT PROJECT
 app.use('/', express.static( path.join(__dirname, '../public') ));
+
+app.use('/api/admin', admin);
 
 app.get('*', function(req,res){
   res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
